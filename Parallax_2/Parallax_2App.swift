0c90837b8203851dc0fa76affccda7344b54aad8
PrathameshBhuravane <bhuravaneprathamesh@gmail.com>
2023-12-21 14:54:44 +0530

    Initial Commit

---
+++ Parallax_2/Parallax_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Parallax_2App.swift
+//  Parallax_2
+//
+//  Created by Prathamesh on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Parallax_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
